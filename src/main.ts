6a6652bf31b38157ba128225ee139dbbf977cb0e
adirifta <adiriftadk@gmail.com>
2025-10-26 23:44:20 +0700

    test main

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
     transform: true,
   }));
 
-  const port = process.env.PORT;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   
   console.log(`🚀 Application is running on: http://localhost:${port}/api`);
