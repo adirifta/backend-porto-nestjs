d21e8ad983dfaa272e74f30276608c866e69d3db
adirifta <adiriftadk@gmail.com>
2025-10-26 13:06:44 +0700

    test deploy

---
+++ src/test/setup.ts
@@ -0,0 +1,11 @@
+// Basic test setup file
+import { Test } from '@nestjs/testing';
+
+// Global test setup
+beforeAll(async () => {
+  // Setup global test module if needed
+});
+
+afterAll(async () => {
+  // Cleanup
+});
